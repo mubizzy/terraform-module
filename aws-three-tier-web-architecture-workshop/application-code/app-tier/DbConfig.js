1c2fba072066314585184098f374ac177560d1e6
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2024-09-04 14:24:51 +0100

    Add files via upload

---
+++ aws-three-tier-web-architecture-workshop/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'my-rds-instance-1.ct66w20wmfun.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'admin123',
+    DB_DATABASE : 'webappdb'
+});
